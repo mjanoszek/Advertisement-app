9961d57e47238ee2781cfa14a55a81a0b6db4eef
mjanoszek <mjanoszek@protonmail.com>
2023-04-14 09:45:18 +0200

    Improve design and fix fonts weight on particular devices

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -76,7 +76,7 @@ function ProductDetails({ data }: { data: ProductDetail }) {
   return (
     <>
       {!isMobile && <Navbar isAbsolute={false} />}
-      <div className='flex justify-center items-start h-screen lg:px-14  mt-[20vw]'>
+      <div className='flex justify-center items-start h-screen lg:px-14  mt-[20vw] '>
         <div className="flex justify-center items-center w-screen flex-col xl:w-screen lg:items-start lg:mt-10 xl:mt-16">
           <div className="w-screen min-[500px]:mb-10 lg:mb-24">
             <Slider image={data.images} isLarge={true} isMobile={isMobile}/>
@@ -85,21 +85,21 @@ function ProductDetails({ data }: { data: ProductDetail }) {
 
 
 
-          <div className="flex max-md:flex-col self-center z-20 mt-[10vw] lg:mt-[5vw] xl:mt-[12vw] 2xl:mt-[15vw] gap-5 mx-2 md:mx-8 ">
+          <div className="flex max-md:flex-col self-center z-20 mt-[10vw] lg:mt-[5vw] xl:mt-[12vw] 2xl:mt-[15vw] gap-5 mx-2 md:mx-8 xl:mx-16 mb-12">
 
         
-            <div className="flex flex-col self-start gap-2">
+            <div className="flex flex-col self-start gap-2 lg:w-5/6">
               <p className='text-xl xl:text-2xl'>{data.location}</p>
-              <p className='font-semibold '>For Sale</p>
-              <p className='lg:text-xl'>{data.price}</p>
+              <p className='font-medium'>For Sale</p>
+              <p className='lg:text-xl font-medium md:text-lg'>{data.price}</p>
               <div className="flex  flex-row gap-5">
                 <p className='flex gap-2 items-center'><FaBath/>{data.beds}</p>
                 <p className='flex gap-2 items-center'><FaBed/>{data.baths}</p>
                 <p className='flex gap-2 items-center'><SlSizeFullscreen/>{data.area}</p>
               </div>
               <div className="flex flex-col gap-2 mt-2">
-                <p className='font-semibold'>About this Home</p>
-                <p className='text-sm sm:text-md '>{data.description}</p>
+                <p className='font-medium 2xl:text-xl'>About this Home</p>
+                <p className='text-sm sm:text-md 2xl:text-lg '>{data.description}</p>
               </div>
               <div className='flex items-center justify-center mt-5'>
                 <iframe className='rounded-xl h-[350px]' width="100%"   src={`https://www.openstreetmap.org/export/embed.html?bbox=${longitude},${latitude},${longitude},${latitude}&amp;layer=mapnik`}></iframe>
@@ -107,7 +107,7 @@ function ProductDetails({ data }: { data: ProductDetail }) {
             </div>
            
 
-            <form className='flex flex-col self-center text-center gap-2 lg:gap-5 mt-10 lg:ml-5 p-3 max-w-[250px] h-fit border border-gray-400 rounded-md shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)]'>
+            <form className='flex w-[250px] lg:w-[350px] flex-col self-center text-center gap-2 lg:gap-5 mt-10 lg:ml-5 p-3 h-fit border border-gray-400 rounded-md shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)]'>
               <p className='font-bold text-xl xl:text-2xl p-2'>Request for more information</p>
               <input type="text" className='border border-black rounded-sm p-2' placeholder='Name'/>
               <input type="email" className='border border-black rounded-sm p-2' placeholder='Email'/>
