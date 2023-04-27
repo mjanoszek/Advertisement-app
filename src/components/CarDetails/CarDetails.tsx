d71cca50de1e515f1ffbd38e26f34b07bf2550d8
mjanoszek <mjanoszek@protonmail.com>
2023-04-27 19:09:50 +0200

    Fix moving content when the button is clicked

--- src/components/CarDetails/CarDetails.tsx
+++ src/components/CarDetails/CarDetails.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 // import { useMediaQuery } from 'react-responsive';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion';
 import Navbar from '../Navbar/Navbar';
 import plaid1Mobile from '../../assets/Images/Cars/Tesla/plaid1Mobile.jpeg';
 import plaid3Mobile from '../../assets/Images/Cars/Tesla/plaid3Mobile.jpeg';
@@ -13,6 +13,10 @@ import plaidVideo5 from '../../assets/Images/Cars/Tesla/plaidVideo5.mp4';
 
 
 function CarDetails({ data }) {
+  const [selected, setSelected] = useState(0);
+
+
+
 
   const teslaData = [
     {
@@ -85,76 +89,80 @@ function CarDetails({ data }) {
 
 
 
-      <div className="flex justify-center flex-col h-screen bg-black" >
-        <div className="flex flex-col text-center self-center text-white mt-36 h-screen  ">
-          <div className="flex flex-col gap-10 ">
-            <div className="relative">
-              <ReactPlayer
-                className="h-full w-full object-cover "
-                url={teslaData[current].url}
-                playing={true}
-                muted={true}
-                loop={true}
-                width='100%'
-                height='100%'
-              />
+      <div className="flex justify-center flex-col h-screen bg-black " >
+        <div className="flex flex-col text-center self-center text-white h-screen relative mt-16">
 
+          <div className="absolute top-10">
+            <AnimatePresence mode='wait'>
+              <motion.div
+                key={current}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ ease: 'easeInOut', duration: 0.3 }}
+              >
+                <ReactPlayer
+                  className="h-full w-full object-cover "
+                  url={teslaData[current].url}
+                  playing={true}
+                  muted={true}
+                  loop={true}
+                  width='100%'
+                  height='100%'
+                
+                />
+
+
+              </motion.div>
+            </AnimatePresence>
+          </div>
+             
+                
               
-              <div className="flex flex-col justify-start gap-10 absolute top-64">
-                <div className="flex justify-start px-4 ">
-                  {teslaData.map((item, indx) => (
-                    <>
-
-                      <button
-                        key={indx}
-                        className={`mx-2 rounded-full h-3 w-3 border-2 border-transparent bg-gray-500   ${
-                          indx === current && 'bg-gray-100'
-                        }`}
-                        onClick={() => {
-                          handleVideoChange(indx);
-                        }}
-                      
-                      />
-                      {indx === current ? (
-                        <motion.div className="underline bg-white w-3 h-3 rounded-full " layoutId="underline" />
-                      ) : null}
-                    </>
-
-                  ))}
-
-
-
-                  {/* {tabs.map((item) => (
-                    <li
-                      key={item.label}
-                      className={item === selectedTab ? 'selected' : ''}
-                      onClick={() => setSelectedTab(item)}
-                    >
-                      {`${item.icon} ${item.label}`}
-                      {item === selectedTab ? (
-                        <motion.div className="underline" layoutId="underline" />
-                      ) : null}
-                    </li>
-                  ))} */}
-
-
-
-
-
-
+          <div className="flex flex-col justify-start gap-10 mt-64 ">
+            <div className="flex justify-start px-4">
+              {teslaData.map((item, indx) => (
+                <div key={indx} className="relative h-full">
+                  <button
+                    className={`mx-2 rounded-full h-3 w-3 border-2 border-transparent bg-gray-500 ${
+                      indx === current && 'bg-gray-100'
+                    }`}
+                    onClick={() => {
+                      handleVideoChange(indx);
+                    }}
+                  />
+                  {indx === current ? (
+                    <motion.div
+                      className="bg-white w-3 h-3 rounded-full absolute mx-2 "
+                      layoutId="underline"
+                      transition={{
+                        type: 'tween',
+                        ease: 'easeInOut',
+                        duration: 0.3,
+                      }}
+                      style={{
+                        top: '31%',
+                      }}
+                    />
+                  ) : null}
                 </div>
-                <div className="flex flex-col text-left bg-opacity-50 px-6 gap-4 ">
-                  <p className="text-md font-medium text-white">{teslaData[current].heading}</p>
-                  <p className="text-sm text-white">{teslaData[current].text}</p>
-                </div>
-              </div>
+              ))}
             </div>
 
+            
 
-       
 
-        
+
+
+            <div className="flex flex-col text-left bg-opacity-50 px-6 gap-4">
+              <p className="text-md font-medium text-white">{teslaData[current].heading}</p>
+              <p className="text-sm text-white">{teslaData[current].text}</p>
+            </div>
           </div>
+
+
+
+          
         </div>
       </div>
       
