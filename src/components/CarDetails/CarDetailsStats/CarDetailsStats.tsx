d7b6564b7e1ec45363951ee6565bf4acea5ed5f3
mjanoszek <mjanoszek@protonmail.com>
2023-06-12 18:16:42 +0200

    Customize heading and paragraph styles on different devices

--- src/components/CarDetails/CarDetailsStats/CarDetailsStats.tsx
+++ src/components/CarDetails/CarDetailsStats/CarDetailsStats.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { IconType } from 'react-icons/lib';
+
+
 
 interface CarDetailsStatsProps {
-  heading: string,
+  heading: string | IconType,
   paragraph: string
 }
+
+
 function CarDetailsStats({ heading, paragraph }: CarDetailsStatsProps) {
+  
   return (
-    <div className="flex flex-col">
-      <p className='font-medium text-md'>{heading}</p>
-      <p className='text-xs'>{paragraph}</p>
+    <div className="flex flex-col items-center justify-center gap-2">
+      {typeof heading === 'string' ? (
+        <p className="text-base sm:text-2xl font-medium">{heading}</p>
+      ) : (
+        <>{heading}</>
+      )}
+        
+      
+      <p className='text-xs sm:text-base'>{paragraph}</p>
     </div>
   );
 }
