5fc8e93a7e54ae0a9af8a11ef120a55bebbe6039
mjanoszek <mjanoszek@protonmail.com>
2023-06-19 19:48:28 +0200

    Optimize for responsive design

--- src/components/CarDetails/CarDetailsVideoCarousel/CarDetailsVideoCarousel.tsx
+++ src/components/CarDetails/CarDetailsVideoCarousel/CarDetailsVideoCarousel.tsx
@@ -3,9 +3,15 @@ import { motion, AnimatePresence } from 'framer-motion';
 import ReactPlayer from 'react-player';
 
 
+interface CarDetailsVideoCarouselProps {
+  videoData: {
+    url: string;
+    heading: string;
+    text: string;
+  }[];
+}
 
-
-function CarDetailsVideoCarousel({ videoData }) {
+function CarDetailsVideoCarousel({ videoData }: CarDetailsVideoCarouselProps ) {
   const [currentVideo, setCurrentVideo] = useState(0);
 
   const handleVideoChange = (index: number) => {
@@ -13,8 +19,8 @@ function CarDetailsVideoCarousel({ videoData }) {
   };
 
   return (
-    <div>
-      <div className="absolute top-10">
+    <div> 
+      <div className="absolute top-10 min-[600px]:px-4">
         <AnimatePresence mode='wait'>
           <motion.div
             key={currentVideo}
@@ -37,44 +43,47 @@ function CarDetailsVideoCarousel({ videoData }) {
           </motion.div>
         </AnimatePresence>
       </div>
-             
-                
-              
-      <div className="flex flex-col justify-start gap-10 mt-64">
-        <div className="flex justify-start px-4">
-          {videoData.map((item, indx) => (
-            <div key={indx} className="relative h-full">
-              <button
-                className={`mx-2 rounded-full h-3 w-3 border-2 border-transparent bg-gray-500 ${
-                  indx === currentVideo && 'bg-gray-100'
-                }`}
-                onClick={() => {
-                  handleVideoChange(indx);
-                }}
-              />
-              {indx === currentVideo ? (
-                <motion.div
-                  className="bg-white w-3 h-3 rounded-full absolute mx-2 "
-                  layoutId="underline"
-                  transition={{
-                    type: 'tween',
-                    ease: 'easeInOut',
-                    duration: 0.3,
-                  }}
-                  style={{
-                    top: '31%',
-                  }}
-                />
-              ) : null}
-            </div>
-          ))}
-        </div>
 
-        <div className="flex flex-col text-left bg-opacity-50 px-6 gap-4">
-          <p className="text-md font-medium text-white">{videoData[currentVideo].heading}</p>
-          <p className="text-sm text-white">{videoData[currentVideo].text}</p>
+
+
+     
+      <div className="flex justify-center items-center">
+        <div className="flex gap-10 mt-[65vw] md:mt-[55vw]">
+          <div className="flex flex-col px-4 min-[600px]:px-8 min-[900px]:px-[30vw]">
+            <div className="flex py-8 ">
+              {videoData.map((item, indx) => (
+                <div key={indx} className="relative h-full flex">
+                  <button
+                    className={`mx-2 rounded-full h-3 w-3 border-2 border-transparent bg-gray-500 ${
+                      indx === currentVideo && 'bg-gray-100'
+                    }`}
+                    onClick={() => {
+                      handleVideoChange(indx);
+                    }}
+                  />
+                  {indx === currentVideo ? (
+                    <motion.div
+                      className="bg-white w-3 h-3 rounded-full absolute mx-2"
+                      layoutId="underline"
+                      transition={{
+                        type: 'tween',
+                        ease: 'easeInOut',
+                        duration: 0.3,
+                      }}
+                    />
+                  ) : null}
+                </div>
+              ))}
+            </div>
+
+            <div className="flex flex-col text-left bg-opacity-50 px-2 gap-4">
+              <p className="text-md font-medium text-white">{videoData[currentVideo].heading}</p>
+              <p className="text-sm text-white">{videoData[currentVideo].text}</p>
+            </div>
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
