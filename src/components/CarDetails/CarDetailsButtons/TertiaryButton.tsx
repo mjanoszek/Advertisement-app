bcc969918cc60e59af79c14ccce22495f4da9509
mjanoszek <mjanoszek@protonmail.com>
2023-07-17 11:07:50 +0200

    Create Tertiary Button

---
+++ src/components/CarDetails/CarDetailsButtons/TertiaryButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TertiaryButton() {
+  return (
+    <button
+      type="button"
+      className='border-2 rounded-md w-full p-2 px-11 mt-2 font-medium min-[600px]:w-fit  bg-gray-100 border-none text-gray-800' >
+      Find My Route
+    </button>
+  );
+}
+
+export default TertiaryButton;
