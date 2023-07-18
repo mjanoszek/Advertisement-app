4460431727139a92398569b174a4befd843d4701
mjanoszek <mjanoszek@protonmail.com>
2023-07-18 09:29:02 +0200

    Import new images and videos

--- src/assets/Images/Cars/Tesla/plaidImport.tsx
+++ src/assets/Images/Cars/Tesla/plaidImport.tsx
@@ -1,38 +1,53 @@
 
 export { default as plaid1 } from './plaid1.jpeg';
+export { default as plaid2 } from './plaid2.jpeg';
 export { default as plaid3 } from './plaid3.jpeg';
+export { default as plaid4 } from './plaid4.jpeg';
+export { default as plaid6 } from './plaid6.jpeg';
+export { default as plaid5 } from './plaid5.jpeg';
+export { default as plaid7 } from './plaid7.jpeg';
+
+export { default as plaid1Desktop } from './plaid1Desktop.jpeg';
+export { default as plaid2Desktop } from './plaid2Desktop.jpeg';
+export { default as plaid3Desktop } from './plaid3Desktop.jpeg';
+export { default as plaid4Desktop } from './plaid4Desktop.jpeg';
+export { default as plaid5Desktop } from './plaid5Desktop.jpeg';
+export { default as plaid6Desktop } from './plaid6Desktop.jpeg';
+export { default as plaid7Desktop } from './plaid7Desktop.jpeg';
+
 export { default as plaidVideo1 } from './plaidVideo1.mp4';
-export { default as plaidVideo1Desktop } from './plaidVideo1Desktop.mp4';
-export { default as plaidVideo2Desktop } from './plaidVideo2Desktop.mp4';
-export { default as plaidVideo3Desktop } from './plaidVideo3Desktop.mp4';
-export { default as plaidVideo4Desktop } from './plaidVideo4Desktop.mp4';
-export { default as plaidVideo5Desktop } from './plaidVideo5Desktop.mp4';
 export { default as plaidVideo2 } from './plaidVideo2.mp4';
 export { default as plaidVideo3 } from './plaidVideo3.mp4';
 export { default as plaidVideo4 } from './plaidVideo4.mp4';
 export { default as plaidVideo5 } from './plaidVideo5.mp4';
 export { default as plaidVideo6 } from './plaidVideo6.mp4';
-export { default as plaid4 } from './plaid4.jpeg';
-export { default as plaid6 } from './plaid6.jpeg';
-export { default as plaid5 } from './plaid5.jpeg';
-export { default as plaid7 } from './plaid7.jpeg';
+
+export { default as plaidVideo1Desktop } from './plaidVideo1Desktop.mp4';
+export { default as plaidVideo2Desktop } from './plaidVideo2Desktop.mp4';
+export { default as plaidVideo3Desktop } from './plaidVideo3Desktop.mp4';
+export { default as plaidVideo4Desktop } from './plaidVideo4Desktop.mp4';
+export { default as plaidVideo5Desktop } from './plaidVideo5Desktop.mp4';
+export { default as plaidVideo6Desktop } from './plaidVideo6Desktop.mp4';
+
 export { default as plaidStyle } from './plaidStyle.jpeg';
 export { default as plaidStyleDesktop } from './plaidStyleDesktop.jpeg';
 export { default as plaidWheelsDesktop } from './plaidWheelsDesktop.jpeg';
 export { default as plaidWheels } from './plaidWheels.jpeg';
 export { default as plaidAerodynamics } from './plaidAerodynamics.jpeg';
 export { default as plaidAerodynamicsDesktop } from './plaidAerodynamicsDesktop.jpeg';
+
 export { default as plaidPowertrain1 } from './plaidPowertrain1.jpeg';
 export { default as plaidPowertrain1Desktop } from './plaidPowertrain1Desktop.jpeg';
 export { default as plaidPowertrain2 } from './plaidPowertrain2.jpeg';
 export { default as plaidPowertrain2Desktop } from './plaidPowertrain2Desktop.jpeg';
+
 export { default as amsterdamToBrussels } from './amsterdamToBrussels.svg';
 export { default as brusselsToParis } from './brusselsToParis.svg';
 export { default as munichToZurich } from './munichToZurich.svg';
 export { default as osloToGothenburg } from './osloToGothenburg.svg';
-export { default as plaidModel } from './plaidmodel.jpeg';
+export { default as plaidModel } from './plaidModel.jpeg';
 export { default as plaidAutopilot } from './plaidAutopilot.jpeg';
+export { default as plaidAutopilotDesktop } from './plaidAutopilotDesktop.jpeg';
 export { default as plaidVideo7Desktop } from './plaidVideo7Desktop.mp4';
 export { default as plaidVideo7 } from './plaidVideo7.mp4';
-export { default as plaid3Desktop } from './plaid3Desktop.jpeg';
 
