047c8077533a34a97caa38508aee9538e4693aaa
mjanoszek <mjanoszek@protonmail.com>
2023-07-18 08:58:47 +0200

    Make the component responsive

--- src/components/CarDetails/CarDetailsStats/CarDetailsStats.tsx
+++ src/components/CarDetails/CarDetailsStats/CarDetailsStats.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { IconType } from 'react-icons/lib';
 
-
-
 interface CarDetailsStatsProps {
   heading: string | IconType,
   paragraph: string
 }
 
-
 function CarDetailsStats({ heading, paragraph }: CarDetailsStatsProps) {
   
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
+    <div className="flex flex-col items-center justify-center gap-2 ">
       {typeof heading === 'string' ? (
-        <p className="text-base sm:text-2xl font-medium">{heading}</p>
+        <p className="text-base min-[600px]:text-2xl min-[900px]:text-xl font-medium">{heading}</p>
       ) : (
         <>{heading}</>
       )}
         
       
-      <p className='text-xs sm:text-base'>{paragraph}</p>
+      <p className='text-xs sm:text-base min-[900px]:text-[0.8rem]'>{paragraph}</p>
     </div>
   );
 }
