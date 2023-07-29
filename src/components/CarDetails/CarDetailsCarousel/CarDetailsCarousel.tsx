2aab52515662549d39c519ea635459a59764208e
mjanoszek <mjanoszek@protonmail.com>
2023-07-29 13:38:24 +0200

    chore: Add new prop and show data depending on it

--- src/components/CarDetails/CarDetailsCarousel/CarDetailsCarousel.tsx
+++ src/components/CarDetails/CarDetailsCarousel/CarDetailsCarousel.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
 interface CarDetailsCarouselProps {
-  carouselImages: Array<{
+  type: 'images' | 'videos';
+  carouselData: Array<{
     name?: string
     url: string,
     heading?: string,
@@ -17,12 +19,12 @@ interface CarDetailsCarouselProps {
 }
 
 
-const CarDetailsCarousel = ({ carouselImages }: CarDetailsCarouselProps) => {
+const CarDetailsCarousel = ({ carouselData, type }: CarDetailsCarouselProps) => {
   
-  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+  const [selecteddataIndex, setSelecteddataIndex] = useState(0);
 
   const handleButtonClick = (indx: number) => {
-    setSelectedImageIndex(indx);
+    setSelecteddataIndex(indx);
   };
 
   const renderArrow = () => null; 
@@ -39,35 +41,50 @@ const CarDetailsCarousel = ({ carouselImages }: CarDetailsCarouselProps) => {
         className="max-w-full"
         renderArrowNext={renderArrow}
         renderArrowPrev={renderArrow}
-        selectedItem={selectedImageIndex}
+        selectedItem={selecteddataIndex}
       >
-        {carouselImages.map((image, indx: number) => (
+        {carouselData.map((data, indx: number) => (
           <div className="carousel-item-wrapper " key={indx}>
             <div className="carousel-item-full-bleed flex justify-center items-center">
-              <img src={image.url} alt={image.name ? image.name : image.heading } className="carousel-item-image" />
+              {type === 'images' ? 
+                <img src={data.url} alt={data.name ? data.name : data.heading } className="carousel-item-data" />
+                :
+                <ReactPlayer
+                  className="h-full w-full object-cover "
+                  url={data.url}
+                  playing={true}
+                  muted={true}
+                  loop={true}
+                  width='100%'
+                  height='100%'
+                
+                />
+              }
             </div>
-            <div className="px-5 w-3/4 ">
+            <div className="px-5 w-full flex flex-col ">
               <div className="carousel-button-line bg-black h-[4px] mt-4 "></div>
-              <div className="carousel-button-content flex flex-col items-start mt-5 ">
+              <div className="carousel-button-content flex flex-col items-start mt-5 mx-2">
                 <button
-                  className="carousel-button text-black font-medium py-2 rounded mb-2 text-left"
+                  className="carousel-button text-black font-medium py-2 rounded text-left"
                   onClick={() => handleButtonClick(indx)}
                 >
-                  {image.name && image.name}
+                  {data.name && data.name}
                 </button>
                 <button className="carousel-button text-black  rounded text-2xl text-left font-medium">
-                  {image.distance && image.distance}
+                  {data.distance && data.distance}
                 </button>
-                <div className="w-full flex flex-col text-left">
+                <div className="w-full flex flex-col text-start">
                   <h2 className="text-black font-bold mb-2 text-left">
-                    {image.heading && image.heading}
+                    {data.heading && data.heading}
                   </h2>
-                  {'range' in image && 
+                  {data.text &&  <p className="text-black mb-2 text-sm">{data.text}</p>}
+                  
+                  {'range' in data && 
                   <div className="flex flex-col ">
-                    <p className="text-black mb-2 text-sm">{image.text}</p>
-                    <p className="text-black text-lg">{image.zeroToHundred}</p>
+                    <p className="text-black mb-2 text-sm">{data.text}</p>
+                    <p className="text-black text-lg">{data.zeroToHundred}</p>
                     <p className='text-gray-800 text-xs'>0-100 km/h</p>
-                    <p className="text-black text-lg">{image.range}</p>
+                    <p className="text-black text-lg">{data.range}</p>
                     <p className='text-gray-800 text-xs'>Range (WLTP)</p>
                   </div>
                   }
