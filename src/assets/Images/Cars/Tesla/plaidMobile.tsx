1dc83bf477eddef0fcc5d7a6881d20ba5f84559e
mjanoszek <mjanoszek@protonmail.com>
2023-07-29 13:29:57 +0200

    assets: add plaid images for mobile

---
+++ src/assets/Images/Cars/Tesla/plaidMobile.tsx
@@ -0,0 +1,12 @@
+import plaid1 from './plaid1Desktop.jpeg';
+import plaid2 from './plaid2Desktop.jpeg';
+import plaid3 from './plaid3Desktop.jpeg';
+import plaid4 from './plaid4Desktop.jpeg';
+import plaid5 from './plaid5Desktop.jpeg';
+import plaid6 from './plaid6Desktop.jpeg';
+import plaid7 from './plaid7Desktop.jpeg';
+
+
+export const plaidMobile = [
+  plaid1, plaid2, plaid3, plaid4, plaid5, plaid6, plaid7,
+];
