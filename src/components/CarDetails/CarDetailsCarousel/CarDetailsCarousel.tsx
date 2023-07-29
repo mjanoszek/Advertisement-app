b40fd9a0c6614b8e08cdd535a56e95b4348eae06
mjanoszek <mjanoszek@protonmail.com>
2023-07-29 10:03:58 +0200

    feat: add interface

--- src/components/CarDetails/CarDetailsCarousel/CarDetailsCarousel.tsx
+++ src/components/CarDetails/CarDetailsCarousel/CarDetailsCarousel.tsx
@@ -2,7 +2,23 @@ import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const ResponsiveCarousel = ({ images }) => {
+
+interface CarDetailsCarouselProps {
+  carouselImages: Array<{
+    name?: string
+    url: string,
+    heading?: string,
+    text?: string,
+    zeroToHundred?: string
+    range?: string
+    distance?: string
+  }>;
+  
+}
+
+
+const CarDetailsCarousel = ({ carouselImages }: CarDetailsCarouselProps) => {
+  
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
   const handleButtonClick = (indx: number) => {
@@ -12,7 +28,7 @@ const ResponsiveCarousel = ({ images }) => {
   const renderArrow = () => null; 
 
   return (
-    <div className="flex items-center justify-center w-[100vw]">
+    <div className="flex items-center justify-center w-full">
       <Carousel
         showThumbs={false}
         showStatus={false}
@@ -25,29 +41,29 @@ const ResponsiveCarousel = ({ images }) => {
         renderArrowPrev={renderArrow}
         selectedItem={selectedImageIndex}
       >
-        {images.map((image, indx: number) => (
-          <div className="carousel-item-wrapper" key={indx}>
+        {carouselImages.map((image, indx: number) => (
+          <div className="carousel-item-wrapper " key={indx}>
             <div className="carousel-item-full-bleed flex justify-center items-center">
-              <img src={image.url} alt={image.name} className="carousel-item-image" />
+              <img src={image.url} alt={image.name ? image.name : image.heading } className="carousel-item-image" />
             </div>
-            <div className="px-5">
-              <div className="carousel-button-line bg-black h-[4px] mt-4 w-3/4"></div>
+            <div className="px-5 w-3/4 ">
+              <div className="carousel-button-line bg-black h-[4px] mt-4 "></div>
               <div className="carousel-button-content flex flex-col items-start mt-5 ">
                 <button
                   className="carousel-button text-black font-medium py-2 rounded mb-2 text-left"
                   onClick={() => handleButtonClick(indx)}
                 >
-                  {image.name}
+                  {image.name && image.name}
                 </button>
                 <button className="carousel-button text-black  rounded text-2xl text-left font-medium">
-                  {image.distance}
+                  {image.distance && image.distance}
                 </button>
-                <div className="w-full p-4 flex flex-col text-left">
+                <div className="w-full flex flex-col text-left">
                   <h2 className="text-black font-bold mb-2 text-left">
-                    {image.heading}
+                    {image.heading && image.heading}
                   </h2>
                   {'range' in image && 
-                  <div className="flex flex-col">
+                  <div className="flex flex-col ">
                     <p className="text-black mb-2 text-sm">{image.text}</p>
                     <p className="text-black text-lg">{image.zeroToHundred}</p>
                     <p className='text-gray-800 text-xs'>0-100 km/h</p>
@@ -61,8 +77,9 @@ const ResponsiveCarousel = ({ images }) => {
           </div>
         ))}
       </Carousel>
+      {}
     </div>
   );
 };
 
-export default ResponsiveCarousel;
+export default CarDetailsCarousel;
