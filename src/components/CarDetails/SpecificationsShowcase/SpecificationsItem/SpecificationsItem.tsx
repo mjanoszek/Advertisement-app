2f17e354168ed32c9a82d4fa20faba4493408fb2
mjanoszek <mjanoszek@protonmail.com>
2023-07-30 18:28:36 +0200

    feat: Create SpecificationsItem component

---
+++ src/components/CarDetails/SpecificationsShowcase/SpecificationsItem/SpecificationsItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface SpecificationsItemProps {
+  heading: string;
+  description: string | React.ReactNode;
+}
+
+function SpecificationsItem({ heading, description }: SpecificationsItemProps) {
+  return (
+    <div className='flex flex-col gap-1 text-white'>
+      <div className='mb-2 h-[0.25px] w-1/5 bg-gray-400'></div>
+      <p className='text-sm font-semibold'>{heading}</p>
+      <p className='text-sm font-medium'>{description}</p>
+    </div>
+  );
+}
+
+export default SpecificationsItem;
