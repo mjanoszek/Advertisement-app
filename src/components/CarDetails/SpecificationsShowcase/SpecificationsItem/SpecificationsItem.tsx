531496205fc2ccfaf613f80134bf7226c8f645d7
mjanoszek <mjanoszek@protonmail.com>
2023-07-31 08:41:50 +0200

    style: update font weight

--- src/components/CarDetails/SpecificationsShowcase/SpecificationsItem/SpecificationsItem.tsx
+++ src/components/CarDetails/SpecificationsShowcase/SpecificationsItem/SpecificationsItem.tsx
@@ -9,8 +9,8 @@ function SpecificationsItem({ heading, description }: SpecificationsItemProps) {
   return (
     <div className='flex flex-col gap-1 text-white'>
       <div className='mb-2 h-[0.25px] w-1/5 bg-gray-400'></div>
-      <p className='text-sm font-semibold'>{heading}</p>
-      <p className='text-sm font-medium'>{description}</p>
+      <p className='text-sm font-medium'>{heading}</p>
+      <p className='text-sm font-normal'>{description}</p>
     </div>
   );
 }
