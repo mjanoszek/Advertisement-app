a2a79b2c56179e3af797e97e0104d0ff291f2607
mjanoszek <mjanoszek@protonmail.com>
2023-07-31 18:41:33 +0200

    style: Remove unnecessary styles

--- src/components/CarDetails/CarDetailsButtons/PrimaryButton.tsx
+++ src/components/CarDetails/CarDetailsButtons/PrimaryButton.tsx
@@ -4,7 +4,7 @@ function PrimaryButton() {
   return (
     <button
       type='button'
-      className='mt-2 w-full rounded-md border-2 border-black bg-white p-2 px-14 font-medium text-black min-[600px]:w-fit min-[600px]:px-24'
+      className='mt-2 w-auto rounded-md border-2 border-black bg-white p-1 px-6 text-lg font-medium text-black'
     >
       Order now
     </button>
