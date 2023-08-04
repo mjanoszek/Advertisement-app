d73ea97a3618c9a9d232f284991d89e3b47f64b4
mjanoszek <mjanoszek@protonmail.com>
2023-08-04 17:53:02 +0200

    docs: add images

---
+++ src/assets/Images/Watches/Submariner/submariner.tsx
@@ -0,0 +1,6 @@
+import submariner1 from './submariner1.jpeg';
+import submariner2 from './submariner2.jpeg';
+import submariner3 from './submariner3.jpeg';
+import submariner4 from './submariner4.jpeg';
+
+export const submarinerImages = [submariner1, submariner2, submariner3, submariner4];
