67fdbf0711a352cdbe626da02c8b85b1e86911d2
mjanoszek <mjanoszek@protonmail.com>
2023-08-05 19:01:43 +0200

    feat: customize watches page

--- src/pages/Watches.tsx
+++ src/pages/Watches.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import AdvertisementCard from '../components/AdvertisementCard/AdvertisementCard';
 
 function Watches() {
-  return <div>Watches</div>;
+  return (
+    <>
+      <Navbar isAbsolute={false} />
+      <AdvertisementCard type='watches' />
+    </>
+  );
 }
 
 export default Watches;
