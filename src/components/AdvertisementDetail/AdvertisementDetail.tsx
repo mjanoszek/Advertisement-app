8cebb4ddab5e03701598d1b3f2f2177f779f37db
mjanoszek <mjanoszek@protonmail.com>
2023-08-06 09:55:14 +0200

    feat: add watches section

--- src/components/AdvertisementDetail/AdvertisementDetail.tsx
+++ src/components/AdvertisementDetail/AdvertisementDetail.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { PropertyList } from '../../data/PropertyList';
 import { CarsList } from '../../data/CarsList';
-import { FaBed, FaBath } from 'react-icons/fa';
+import { FaBed, FaBath, FaWater } from 'react-icons/fa';
 import { BsSpeedometer } from 'react-icons/bs';
-import { PiEngineBold, PiTimerBold } from 'react-icons/pi';
+import {
+  PiCircleHalfTiltFill,
+  PiEngineBold,
+  PiTimerBold,
+} from 'react-icons/pi';
 import { SlSizeFullscreen } from 'react-icons/sl';
 import { Slider } from '../Slider/Slider';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import WatchesList from '../../data/WatchesList';
+import { MdWatch } from 'react-icons/md';
 
 interface AdvertisementDetailProps {
   advertisementType: string;
@@ -22,22 +28,22 @@ function AdvertisementDetail({ advertisementType }: AdvertisementDetailProps) {
           <Link
             to={`/Properties/${item.propertyID}`}
             key={indx}
-            className='max-sm:items-left flex w-[250px] flex-col rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] max-sm:pb-2 lg:w-[340px] lg:max-w-[340px] xl:w-full'
+            className='max-sm:items-left flex w-[250px] flex-col rounded-md pt-12 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] max-sm:pb-2 lg:w-[340px] lg:max-w-[340px] xl:w-full'
           >
-            <Slider image={item.images} isMobile={isMobile} />
-            <div className='items-start  justify-between gap-5 px-3 sm:flex sm:flex-col'>
-              <p className='max-[375px]:text-sm text-base'>{item.title}</p>
+            <Slider image={item.images} isMobile={isMobile} isInMenu={true} />
+            <div className='items-start justify-between gap-5 px-3 max-lg:py-5 sm:flex sm:flex-col'>
+              <p className='text-base max-[375px]:text-sm'>{item.title}</p>
               <div className='flex flex-col'>
                 <p className='font-bold'>{item.price}</p>
-                <p className='flex gap-2'>
+                <p className='flex items-center gap-2'>
                   <FaBed />
                   {item.beds}
                 </p>
-                <p className='flex gap-2'>
+                <p className='flex items-center gap-2'>
                   <FaBath />
                   {item.baths}
                 </p>
-                <p className='flex gap-2'>
+                <p className='flex items-center gap-2'>
                   <SlSizeFullscreen />
                   {item.area}
                 </p>
@@ -53,10 +59,10 @@ function AdvertisementDetail({ advertisementType }: AdvertisementDetailProps) {
           <Link
             to={`/Cars/${item.carID}`}
             key={indx}
-            className='max-sm:items-left flex w-[250px] flex-col rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] max-sm:pb-2 lg:w-[340px] lg:min-w-[340px] lg:max-w-[340px] xl:w-full '
+            className='max-sm:items-left flex w-[250px] flex-col rounded-md pt-12 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] max-sm:pb-2 lg:w-[340px] lg:min-w-[340px] lg:max-w-[340px] xl:w-full'
           >
-            <Slider image={item.images} isMobile={isMobile} />
-            <div className='mt-2 items-start justify-between px-3 pb-3 sm:flex sm:flex-col'>
+            <Slider image={item.images} isMobile={isMobile} isInMenu={true} />
+            <div className='mt-2 items-start justify-between px-3 pb-3 max-lg:py-5 sm:flex sm:flex-col'>
               <p className='max-[375px]:text-md text-base font-semibold'>
                 {item.title}
               </p>
@@ -64,17 +70,49 @@ function AdvertisementDetail({ advertisementType }: AdvertisementDetailProps) {
                 {item.yearOfManufacture}
               </p>
               <div className='flex flex-col'>
-                <p className='flex gap-2'>
+                <p className='flex items-center gap-2'>
                   <BsSpeedometer />
                   {item.topSpeed}
                 </p>
-                <p className='flex gap-2'>
+                <p className='flex items-center gap-2'>
                   <PiEngineBold />
                   {item.power}
                 </p>
-                <p className='flex gap-2'>
+                <p className='flex items-center gap-2'>
                   <PiTimerBold />
                   {item.ZeroToHundred}
+                </p>
+                <hr className='my-2' />
+                <p className='font-medium'>{item.price}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+
+      {advertisementType == 'watches' &&
+        WatchesList.map((item, indx) => (
+          <Link
+            to={`/Watches/${item.watchID}`}
+            key={indx}
+            className='max-sm:items-left flex w-[250px] flex-col rounded-md pt-12 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] max-sm:pb-2 lg:w-[340px] lg:min-w-[340px] lg:max-w-[340px] xl:w-full'
+          >
+            <Slider image={item.images} isMobile={isMobile} isInMenu={true} />
+            <div className='mt-2 items-start justify-between px-3 pb-3 max-lg:py-5 sm:flex sm:flex-col'>
+              <p className='max-[375px]:text-md text-base font-semibold'>
+                {item.title}
+              </p>
+              <div className='flex flex-col'>
+                <p className='flex items-center gap-2'>
+                  <FaWater />
+                  {item.waterResistance}
+                </p>
+                <p className='flex items-center gap-2'>
+                  <PiCircleHalfTiltFill />
+                  {item.diameter}
+                </p>
+                <p className='flex items-center gap-2'>
+                  <MdWatch />
+                  {item.material}
                 </p>
                 <hr className='my-2' />
                 <p className='font-medium'>{item.price}</p>
