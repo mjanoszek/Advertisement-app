e6a5f13bc74db5eaf5105db62516e8bcabdc9293
mjanoszek <mjanoszek@protonmail.com>
2023-08-06 12:55:01 +0200

    feat: use form component

--- src/components/PropertyDetails/PropertyDetails.tsx
+++ src/components/PropertyDetails/PropertyDetails.tsx
@@ -6,6 +6,7 @@ import { SlSizeFullscreen } from 'react-icons/sl';
 import { useMediaQuery } from 'react-responsive';
 import Navbar from '../Navbar/Navbar';
 import { PropertyItem } from '../../types/PropertyItem';
+import AdvertisementForm from '../AdvertisementForm/AdvertisementForm';
 
 interface PropertyDetailsProps {
   data: PropertyItem;
@@ -104,38 +105,7 @@ function PropertyDetails({ data }: PropertyDetailsProps) {
                 ></iframe>
               </div>
             </div>
-
-            <form className='mt-10 flex h-fit w-[250px] flex-col gap-2 self-center rounded-md border border-gray-400 p-3 text-center shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] lg:ml-5 lg:w-[350px] lg:gap-5'>
-              <p className='p-2 text-xl font-medium xl:text-2xl'>
-                Request for more information
-              </p>
-              <input
-                type='text'
-                className='rounded-sm border border-black p-2'
-                placeholder='Name'
-              />
-              <input
-                type='email'
-                className='rounded-sm border border-black p-2'
-                placeholder='Email'
-              />
-              <input
-                type='tel'
-                className='rounded-sm border border-black p-2'
-                placeholder='Your phone'
-              />
-              <textarea
-                className='resize rounded-sm border border-black p-2'
-                rows={parseInt('4')}
-                placeholder='Write a message'
-              ></textarea>
-              <input
-                type='submit'
-                value='Send'
-                className='w-1/3 self-center rounded-sm border border-black p-1'
-              />
-              <div>{showMore ? textToShow : truncatedText}</div>
-            </form>
+            <AdvertisementForm />
           </div>
         </div>
       </div>
--- src/components/WatchesDetails/WatchesDetails.tsx
+++ src/components/WatchesDetails/WatchesDetails.tsx
@@ -8,6 +8,7 @@ import Navbar from '../Navbar/Navbar';
 import { PiCircleHalfTiltFill } from 'react-icons/pi';
 import { MdWatch } from 'react-icons/md';
 import { WatchItem } from '../../types/WatchItem';
+import AdvertisementForm from '../AdvertisementForm/AdvertisementForm';
 
 interface WatchesDetailsProps {
   data: WatchItem;
@@ -78,37 +79,7 @@ function WatchesDetails({ data }: WatchesDetailsProps) {
               </div>
             </div>
 
-            <form className='mt-10 flex h-fit w-[250px] flex-col gap-2 self-center rounded-md border border-gray-400 p-3 text-center shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] lg:ml-5 lg:w-[350px] lg:gap-5'>
-              <p className='p-2 text-xl font-medium xl:text-2xl'>
-                Request for more information
-              </p>
-              <input
-                type='text'
-                className='rounded-sm border border-black p-2'
-                placeholder='Name'
-              />
-              <input
-                type='email'
-                className='rounded-sm border border-black p-2'
-                placeholder='Email'
-              />
-              <input
-                type='tel'
-                className='rounded-sm border border-black p-2'
-                placeholder='Your phone'
-              />
-              <textarea
-                className='resize rounded-sm border border-black p-2'
-                rows={parseInt('4')}
-                placeholder='Write a message'
-              ></textarea>
-              <input
-                type='submit'
-                value='Send'
-                className='w-1/3 self-center rounded-sm border border-black p-1'
-              />
-              <div>{showMore ? textToShow : truncatedText}</div>
-            </form>
+            <AdvertisementForm />
           </div>
         </div>
       </div>
