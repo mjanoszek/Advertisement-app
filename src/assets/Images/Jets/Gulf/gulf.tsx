27ffe08f1d041a6fcc1596c8f870d8f79ba0def1
mjanoszek <mjanoszek@protonmail.com>
2023-08-07 20:08:43 +0200

    docs: add and import jet images

---
+++ src/assets/Images/Jets/Gulf/gulf.tsx
@@ -0,0 +1,6 @@
+import gulf1 from './gulf1.jpeg';
+import gulf2 from './gulf2.jpeg';
+import gulf3 from './gulf3.jpeg';
+import gulf4 from './gulf4.jpeg';
+
+export const gulfImages = [gulf1, gulf2, gulf3, gulf4];
