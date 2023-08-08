9cc3b55af6b2fdc074ae2b2c0ca2734a4ca097e5
mjanoszek <mjanoszek@protonmail.com>
2023-08-08 12:29:22 +0200

    refactor: add type do data and switch on different depending on data passed

--- src/pages/ProductProfile.tsx
+++ src/pages/ProductProfile.tsx
@@ -6,28 +6,40 @@ import { getWatchById } from '../data/WatchesList';
 import PropertyDetails from '../components/PropertyDetails/PropertyDetails';
 import CarDetails from '../components/CarDetails/CarDetails';
 import WatchesDetails from '../components/WatchesDetails/WatchesDetails';
+import { getJetById } from '../data/JetsList';
+import JetsDetails from '../components/JetsDetails/JetsDetails';
+import { PropertyItem } from '../types/PropertyItem';
+import { CarItem } from '../types/CarItem';
+import { WatchItem } from '../types/WatchItem';
+import { JetItem } from '../types/JetItem';
+
+type DataItem = PropertyItem | CarItem | WatchItem | JetItem;
 
 function ProductProfile() {
-  const { carID, propertyID, watchID } = useParams();
+  const { carID, propertyID, watchID, jetID } = useParams();
 
-  let data;
+  let data: DataItem | undefined;
+
   if (propertyID) {
     data = getPropertyById(Number(propertyID));
   } else if (carID) {
     data = getCarById(Number(carID));
   } else if (watchID) {
     data = getWatchById(Number(watchID));
+  } else if (jetID) {
+    data = getJetById(Number(jetID));
   }
 
   return (
     <>
-      {propertyID ? (
-        <PropertyDetails data={data} />
-      ) : carID ? (
-        <CarDetails data={data} />
-      ) : watchID ? (
-        <WatchesDetails data={data} />
-      ) : null}
+      {data && (
+        <>
+          {propertyID && <PropertyDetails data={data as PropertyItem} />}
+          {carID && <CarDetails data={data as CarItem} />}
+          {watchID && <WatchesDetails data={data as WatchItem} />}
+          {jetID && <JetsDetails data={data as JetItem} />}
+        </>
+      )}
     </>
   );
 }
