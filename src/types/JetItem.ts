8df74da3556ab6025a35ac9930c26dbf2db8a82c
mjanoszek <mjanoszek@protonmail.com>
2023-08-08 09:19:08 +0200

    chore:create JetItem interface for PrivateJet representation

---
+++ src/types/JetItem.ts
@@ -0,0 +1,11 @@
+export interface JetItem {
+  jetID: number;
+  title: string;
+  image: string;
+  price: string;
+  maxSpeed: string;
+  range: string;
+  engine: string;
+  description: string;
+  images: string[];
+}
